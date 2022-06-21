d1acbb0b5af5253a4f68c779c37f41289d5ab19a
Диана Ильина <diana.1993.30@gmail.com>
2022-06-21 12:20:21 +0100

    Initial commit

---
+++ src/test/java/ru/netology/DataClass.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+import lombok.Data;
+
+@Data
+public class DataClass {
+    private final String login;
+    private final String password;
+    private final String status;
+}
