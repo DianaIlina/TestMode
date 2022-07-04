c24742207413832621f4303fb9930f1e9eebd046
Диана Ильина <diana.1993.30@gmail.com>
2022-07-04 23:23:11 +0100

    Fixing homework

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,41 +1,59 @@
 package ru.netology;
 
 import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import lombok.experimental.UtilityClass;
 
 import java.util.Locale;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static io.restassured.RestAssured.given;
+
 @UtilityClass
 public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+        .setBaseUri("http://localhost")
+        .setPort(9999)
+        .setAccept(ContentType.JSON)
+        .setContentType(ContentType.JSON)
+        .log(LogDetail.ALL)
+        .build();
+
+    private static final Faker faker = new Faker (new Locale("en"));
+
+    public static String getLogin() {
+        return faker.name().username();
+    }
+    public static String getPassword() {
+        return faker.internet().password();
+    }
+
+    public static void sendRequest(DataClass body) {
+        given()
+            .spec(requestSpec)
+            .body(body)
+            .when()
+            .post("/api/system/users")
+            .then() // "тогда ожидаем"
+            .statusCode(200);
+    }
+
+
     @UtilityClass
-    public static class LogIn {
-        /*
-        public static String GenerateStatus() {
-            final String[] statuses = {"active", "blocked"};
-
-            public String getRandomElement() {
-                return statuses[ThreadLocalRandom.current().nextInt(statuses.length)];
-            }
+    public static class Registration {
+        public static DataClass getRegisteredUser(String status) {
+            DataClass user = getUser(status);
+            sendRequest(user);
+            return user;
         }
-        */
-
-        public static DataClass[] generateUsers() {
-            Faker faker = new Faker (new Locale("en"));
-            DataClass[] users = new DataClass[2];
-            // String status = LogIn.GenerateStatus();
-
-            users[0] = new DataClass(
-                    faker.name().username(),
-                    faker.internet().password(),
-                    "active"
-            );
-            users[1] = new DataClass(
-                    faker.name().username(),
-                    faker.internet().password(),
-                    "blocked"
-            );
-            return users;
+        public static DataClass getUser(String status) {
+            String login = getLogin();
+            String password = getPassword();
+            DataClass user = new DataClass(login, password, status);
+            return user;
         }
     }
 }
--- src/test/java/ru/netology/LogInAutoTest.java
+++ src/test/java/ru/netology/LogInAutoTest.java
@@ -1,67 +1,43 @@
 package ru.netology;
 
 import com.codeborne.selenide.Configuration;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static io.restassured.RestAssured.given;
 
 
 public class LogInAutoTest {
-    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-    private static DataClass[] users;
-
-    @BeforeAll
-    static void setUpAll() {
-        users = DataGenerator.LogIn.generateUsers();
-        for (DataClass user : users) {
-            given()
-                    .spec(requestSpec)
-                    .body(new DataClass(user.getLogin(), user.getPassword(), user.getStatus()))
-                    .when()
-                    .post("/api/system/users")
-                    .then() // "тогда ожидаем"
-                    .statusCode(200);
-        }
-    }
-
-    @Test
-    public void testUserActive() {
-
+    @BeforeEach
+    void setup() {
         Configuration.headless = true;
         open("http://localhost:9999");
+    }
+    @Test
+    public void testUserActive() {
+        DataClass user = DataGenerator.Registration.getRegisteredUser("active");
 
-        $x("//*/span[@data-test-id=\"login\"]//input").setValue(users[0].getLogin());
-        $x("//*/span[@data-test-id=\"password\"]//input").setValue(users[0].getPassword());
+        $x("//*/span[@data-test-id=\"login\"]//input").setValue(user.getLogin());
+        $x("//*/span[@data-test-id=\"password\"]//input").setValue(user.getPassword());
         $x("//*/button[@data-test-id=\"action-login\"]").click();
 
-        $x("//*/div[@class=\"App_appContainer__3jRx1\"]")
-                .shouldHave(visible,Duration.ofSeconds(15));
+        $x("//*/h2[contains(@class,\"heading\")]")
+                .shouldHave(visible,Duration.ofSeconds(15))
+                .shouldHave(text("Личный кабинет"));
     }
 
     @Test
     public void testUserBlocked() {
-        Configuration.headless = true;
+        DataClass user = DataGenerator.Registration.getRegisteredUser("blocked");
 
-        open("http://localhost:9999");
-
-        $x("//*/span[@data-test-id=\"login\"]//input").setValue(users[1].getLogin());
-        $x("//*/span[@data-test-id=\"password\"]//input").setValue(users[1].getPassword());
+        $x("//*/span[@data-test-id=\"login\"]//input").setValue(user.getLogin());
+        $x("//*/span[@data-test-id=\"password\"]//input").setValue(user.getPassword());
         $x("//*/button[@data-test-id=\"action-login\"]").click();
 
         $(byText("Пользователь заблокирован"))
@@ -69,13 +45,25 @@ public class LogInAutoTest {
     }
 
     @Test
-    public void testUserWrongLogin() {
-        Configuration.headless = true;
+    public void testUserDoesntExist() {
+        DataClass user = DataGenerator.Registration.getUser("active");
 
-        open("http://localhost:9999");
+        $x("//*/span[@data-test-id=\"login\"]//input").setValue(user.getLogin());
+        $x("//*/span[@data-test-id=\"password\"]//input").setValue(user.getPassword());
+        $x("//*/button[@data-test-id=\"action-login\"]").click();
 
-        $x("//*/span[@data-test-id=\"login\"]//input").setValue(users[0].getLogin());
-        $x("//*/span[@data-test-id=\"password\"]//input").setValue(users[1].getPassword());
+        $(byText("Неверно указан логин или пароль"))
+                .shouldHave(visible, Duration.ofSeconds(10));
+    }
+
+
+    @Test
+    public void testUserWrongPassword() {
+        DataClass user = DataGenerator.Registration.getRegisteredUser("active");
+        String wrongPassword = DataGenerator.getPassword();
+
+        $x("//*/span[@data-test-id=\"login\"]//input").setValue(user.getLogin());
+        $x("//*/span[@data-test-id=\"password\"]//input").setValue(wrongPassword);
         $x("//*/button[@data-test-id=\"action-login\"]").click();
 
         $(byText("Неверно указан логин или пароль"))
@@ -83,13 +71,12 @@ public class LogInAutoTest {
     }
 
     @Test
-    public void testUserWrongPassword() {
-        Configuration.headless = true;
+    public void testUserWrongLogin() {
+        DataClass user = DataGenerator.Registration.getRegisteredUser("active");
+        String wrongLogin = DataGenerator.getLogin();
 
-        open("http://localhost:9999");
-
-        $x("//*/span[@data-test-id=\"login\"]//input").setValue(users[1].getLogin());
-        $x("//*/span[@data-test-id=\"password\"]//input").setValue(users[0].getPassword());
+        $x("//*/span[@data-test-id=\"login\"]//input").setValue(wrongLogin);
+        $x("//*/span[@data-test-id=\"password\"]//input").setValue(user.getPassword());
         $x("//*/button[@data-test-id=\"action-login\"]").click();
 
         $(byText("Неверно указан логин или пароль"))
